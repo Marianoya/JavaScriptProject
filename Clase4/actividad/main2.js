d57f239c30c9c530bce762a44af07adad142ee3d
Mariano Yanez <marianoya920@gmail.com>
2023-11-04 19:05:45 -0600

    Clase4

--- Clase4/actividad/main2.js
+++ Clase4/actividad/main2.js
@@ -53,7 +53,10 @@ const cambiarNombre = () => {
  const aleatorio = () => Math.floor(Math.random() * 100 / 10) % 3
 
  // devuelve un emoji segun el numero que salga en aleatorio()
- const imprimirManoAleatoria = () => juego.opciones[aleatorio()]
+ const imprimirMano = (opcion) => juego.opciones[opcion]
+
+ ///
+ const imprimirManoAleatoria = () => imprimirMano(aleatorio())
 
  const imprimirResultado = () => {
     // O BIEN TMB PODEMOS USAR
@@ -69,4 +72,51 @@ const cambiarNombre = () => {
     }else{
         return `Fin del juego. Empate`
     }
+ }
+
+ const ganador = (jugador1, jugador2) => {
+    console.log('Opciones: ', jugador1, jugador2);
+
+    if(jugador1 == jugador2){
+        console.log('Empate');
+    } else if(jugador1 ==0 && jugador2 ==1){
+        juego.scores.puntosJ1 += 1;
+        console.log('Gana J1. Tijera vs papel')
+    } else if( jugador2 == 0 && jugador1 ==1){
+    juego.scores.puntosJ2 += 1;
+    console.log('Gana J2. Tijera vs papel')
+ } else if( jugador1 == 1 && jugador2 == 2 ){
+    juego.scores.puntosJ1 += 1;
+    console.log('Gana J1. Tijera vs papel')
+ } else if( jugador2 == 1 && jugador1 == 2 ){
+    juego.scores.puntosJ2 += 1;
+    console.log('Gana J2. Tijera vs papel')
+ } else if( jugador1 == 2 && jugador2 == 0 ){
+    juego.scores.puntosJ1 += 1;
+    console.log('Gana J1. Tijera vs papel')
+ }else if( jugador2 == 2 && jugador1 == 0 ){
+    juego.scores.puntosJ2 += 1;
+    console.log('Gana J2. Tijera vs papel')
+}
+ }
+
+ const jugar = () => {
+    let partidas = 3;
+
+    cambiarNombre()
+
+    while (partidas > 0){
+        console.log('Elige una opcion', juego.opciones);
+        let opcionJugador1 = prompt("Elige una opcion 1-3 \n") 
+        let opcionJugador2 = aleatorio()
+        ganador(opcionJugador1, opcionJugador2)
+        console.log('Jugador1: ' + imprimirMano(opcionJugador1), ' .Computadora: ', imprimirMano(opcionJugador2));
+
+        // vamos descontando partidas
+        partidas--
+
+
+    }
+
+    imprimirResultado()
  }
